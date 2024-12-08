5fa1d8110559d88e49978815d31f2c94125bf693
swagatam408 <mr.swagatam9@gmail.com>
2024-12-08 16:58:17 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+n1 = int(input("enter a number:"))
+n2 = int(input("enter a number:"))
+n3 = int(input("enter a number:"))
+if (n1>n2) and (n1>n3) :
+    print( n1,"the greatest  number")
+elif (n2>n3) and (n2>n1) :
+    print( n2,"is the greatest number")
+else:
+    print( n3,"is the greast number")
+
---
+++ 4.py
@@ -0,0 +1,22 @@
+n = int( input ("enter your number:"))
+if ( n == 1 ):
+    print("it is not a prime number")
+for i in range (2, n):
+    
+
+    if ( n % i == 0 ):
+        print( n,"is not a prime number")
+    else:
+        print( n,"is a prime number")  
+          
+
+
+
+
+
+
+
+
+
+
+    
